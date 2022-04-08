5191845a6068943c20c8592db08519e670cd24a0
wurstsalat <mailtrash@posteo.de>
2022-04-09 00:30:06 +0200

    Fix adding event listener to copy button

--- themes/xmpp-providers/static/js/scripts.js
+++ themes/xmpp-providers/static/js/scripts.js
@@ -3,6 +3,8 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 const badge_code_copy = document.getElementById('badge_code_copy');
-badge_code_copy.addEventListener('click', function() {
-    navigator.clipboard.writeText(document.getElementById('badge_code_input').value);
-});
+if (badge_code_copy) {
+    badge_code_copy.addEventListener('click', function() {
+        navigator.clipboard.writeText(document.getElementById('badge_code_input').value);
+    });
+}
