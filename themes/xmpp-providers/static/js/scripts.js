73c7d5a1d14cd756838ab46c18eac953b411067a
wurstsalat <mailtrash@posteo.de>
2022-04-09 16:09:21 +0200

    Add provider filter box

--- themes/xmpp-providers/static/js/scripts.js
+++ themes/xmpp-providers/static/js/scripts.js
@@ -2,9 +2,26 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-const badge_link_copy = document.getElementById('badge_link_copy');
+const badge_link_copy = document.getElementById("badge_link_copy");
 if (badge_link_copy) {
-    badge_link_copy.addEventListener('click', function() {
-        navigator.clipboard.writeText(document.getElementById('badge_link_input').value);
+    badge_link_copy.addEventListener("click", function() {
+        navigator.clipboard.writeText(document.getElementById("badge_link_input").value);
     });
 }
+
+window.addEventListener("load", function() {
+    const checkboxes = document.querySelectorAll("#status-selector input");
+    const show_hide = function(checkbox) {
+      const property = checkbox.getAttribute("name");
+      if (property == "free") {
+        const relevant_providers = document.querySelectorAll("[data-property-free=false]");
+        relevant_providers.forEach(function(provider) {
+          provider.hidden = checkbox.checked;
+        });
+      };
+      checkbox.addEventListener("click", function(event) {
+        show_hide(event.target);
+      });
+    };
+    checkboxes.forEach(show_hide);
+});
