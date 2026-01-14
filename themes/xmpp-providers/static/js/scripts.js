cee3a1eb8b73cf5c2cc086151187e32850b8b406
wurstsalat <mailtrash@posteo.de>
2026-01-14 21:13:03 +0100

    Scripts: Format with Prettier

--- themes/xmpp-providers/static/js/scripts.js
+++ themes/xmpp-providers/static/js/scripts.js
@@ -17,83 +17,109 @@ function initProviderFilters() {
   // Provider filtering in overview
   const checkboxes = document.querySelectorAll("#status-selector input");
   for (const checkbox of checkboxes) {
-    const filterId = checkbox.id.split("filter_")[1]
-    const filterValue = urlParams.get(filterId)
-    checkbox.checked = filterValue === "true" ? true : false
+    const filterId = checkbox.id.split("filter_")[1];
+    const filterValue = urlParams.get(filterId);
+    checkbox.checked = filterValue === "true" ? true : false;
     checkbox.addEventListener("click", () => {
       urlParams.set(filterId, checkbox.checked);
-      history.pushState(null, "", `${window.location.pathname}?${urlParams.toString()}`);
+      history.pushState(
+        null,
+        "",
+        `${window.location.pathname}?${urlParams.toString()}`
+      );
       filterProviders();
     });
   }
-  const filterServerLocations = document.getElementById("filter_server_locations");
-  filterServerLocations.value = urlParams.get("server_location") ?? "all"
+  const filterServerLocations = document.getElementById(
+    "filter_server_locations"
+  );
+  filterServerLocations.value = urlParams.get("server_location") ?? "all";
   filterServerLocations.addEventListener("change", () => {
     urlParams.set("server_location", filterServerLocations.value);
-    history.pushState(null, "", `${window.location.pathname}?${urlParams.toString()}`);
-    filterProviders()
-  })
+    history.pushState(
+      null,
+      "",
+      `${window.location.pathname}?${urlParams.toString()}`
+    );
+    filterProviders();
+  });
   const filterBusFactor = document.getElementById("filter_bus_factor");
-  filterBusFactor.value = urlParams.get("bus_factor") ?? "all"
+  filterBusFactor.value = urlParams.get("bus_factor") ?? "all";
   filterBusFactor.addEventListener("change", () => {
     urlParams.set("bus_factor", filterBusFactor.value);
-    history.pushState(null, "", `${window.location.pathname}?${urlParams.toString()}`);
-    filterProviders()
-  })
+    history.pushState(
+      null,
+      "",
+      `${window.location.pathname}?${urlParams.toString()}`
+    );
+    filterProviders();
+  });
 
   if (urlParams.size > 0) {
     // Show filter box if there are filter query parameters
     document.getElementById("filter-button").click();
   }
 
-  filterProviders()
+  filterProviders();
 }
 
 function filterProviders() {
-  function intersection (a, b) {
+  function intersection(a, b) {
     const setA = new Set(a);
-    return b.filter(value => setA.has(value));
+    return b.filter((value) => setA.has(value));
   }
 
-  const freeOfChargeOnly = document.getElementById("filter_free_of_charge").checked
-  const professionalHostingOnly = document.getElementById("filter_professional_hosting").checked
-  const ratingGreenWebCheckOnly = document.getElementById("filter_rating_green_web_check").checked
-  const inBandRegistrationOnly = document.getElementById("filter_in_band_registration").checked
-  const passwordResetOnly = document.getElementById("filter_password_reset").checked
-  const serverLocationOnly = document.getElementById("filter_server_locations").value
-  const busFactorOnly = document.getElementById("filter_bus_factor").value
+  const freeOfChargeOnly = document.getElementById(
+    "filter_free_of_charge"
+  ).checked;
+  const professionalHostingOnly = document.getElementById(
+    "filter_professional_hosting"
+  ).checked;
+  const ratingGreenWebCheckOnly = document.getElementById(
+    "filter_rating_green_web_check"
+  ).checked;
+  const inBandRegistrationOnly = document.getElementById(
+    "filter_in_band_registration"
+  ).checked;
+  const passwordResetOnly = document.getElementById(
+    "filter_password_reset"
+  ).checked;
+  const serverLocationOnly = document.getElementById(
+    "filter_server_locations"
+  ).value;
+  const busFactorOnly = document.getElementById("filter_bus_factor").value;
 
-  const allProviders = document.querySelectorAll("[data-provider]")
+  const allProviders = document.querySelectorAll("[data-provider]");
 
-  let freeOfChargeProviders = []
+  let freeOfChargeProviders = [];
   if (freeOfChargeOnly) {
     freeOfChargeProviders = document.querySelectorAll(
       "[data-property-free=true]"
     );
   }
 
-  let professionalHostingProviders = []
+  let professionalHostingProviders = [];
   if (professionalHostingOnly) {
     professionalHostingProviders = document.querySelectorAll(
       "[data-property-professional-hosting=true]"
     );
   }
 
-  let ratingGreenWebCheckProviders = []
+  let ratingGreenWebCheckProviders = [];
   if (ratingGreenWebCheckOnly) {
     ratingGreenWebCheckProviders = document.querySelectorAll(
       "[data-property-rating-green-web-check=true]"
     );
   }
 
-  let inBandRegistrationProviders = []
+  let inBandRegistrationProviders = [];
   if (inBandRegistrationOnly) {
     inBandRegistrationProviders = document.querySelectorAll(
       "[data-property-ibr=true]"
     );
   }
 
-  let passwordResetProviders = []
+  let passwordResetProviders = [];
   if (passwordResetOnly) {
     passwordResetProviders = document.querySelectorAll(
       "[data-property-password-reset=true]"
@@ -101,7 +127,8 @@ function filterProviders() {
   }
 
   let serverLocationProviders = document.querySelectorAll(
-    "[data-property-server-locations]")
+    "[data-property-server-locations]"
+  );
   if (serverLocationOnly !== "all") {
     serverLocationProviders = document.querySelectorAll(
       `[data-property-server-locations*="${serverLocationOnly}"]`
@@ -109,7 +136,8 @@ function filterProviders() {
   }
 
   let busFactorProviders = document.querySelectorAll(
-    "[data-property-bus-factor]")
+    "[data-property-bus-factor]"
+  );
   if (busFactorOnly !== "all") {
     if (busFactorOnly === "ge_1") {
       busFactorProviders = document.querySelectorAll(
@@ -121,41 +149,61 @@ function filterProviders() {
         `:not([data-property-bus-factor="-1"], [data-property-bus-factor="1"])`
       );
     }
-
   }
 
-  let filteredProviders = Array.from(allProviders)
+  let filteredProviders = Array.from(allProviders);
 
   if (freeOfChargeOnly) {
-    filteredProviders = intersection(filteredProviders, Array.from(freeOfChargeProviders))
+    filteredProviders = intersection(
+      filteredProviders,
+      Array.from(freeOfChargeProviders)
+    );
   }
   if (professionalHostingOnly) {
-    filteredProviders = intersection(filteredProviders, Array.from(professionalHostingProviders))
+    filteredProviders = intersection(
+      filteredProviders,
+      Array.from(professionalHostingProviders)
+    );
   }
   if (ratingGreenWebCheckOnly) {
-    filteredProviders = intersection(filteredProviders, Array.from(ratingGreenWebCheckProviders))
+    filteredProviders = intersection(
+      filteredProviders,
+      Array.from(ratingGreenWebCheckProviders)
+    );
   }
   if (inBandRegistrationOnly) {
-    filteredProviders = intersection(filteredProviders, Array.from(inBandRegistrationProviders))
+    filteredProviders = intersection(
+      filteredProviders,
+      Array.from(inBandRegistrationProviders)
+    );
   }
   if (passwordResetOnly) {
-    filteredProviders = intersection(filteredProviders, Array.from(passwordResetProviders))
+    filteredProviders = intersection(
+      filteredProviders,
+      Array.from(passwordResetProviders)
+    );
   }
   if (serverLocationOnly) {
-    filteredProviders = intersection(filteredProviders, Array.from(serverLocationProviders))
+    filteredProviders = intersection(
+      filteredProviders,
+      Array.from(serverLocationProviders)
+    );
   }
   if (busFactorOnly) {
-    filteredProviders = intersection(filteredProviders, Array.from(busFactorProviders))
+    filteredProviders = intersection(
+      filteredProviders,
+      Array.from(busFactorProviders)
+    );
   }
 
   for (const provider of allProviders) {
     if (filteredProviders.includes(provider)) {
-      provider.hidden = false
+      provider.hidden = false;
     } else {
-      provider.hidden = true
+      provider.hidden = true;
     }
   }
-};
+}
 
 function initCopyBadgeButton() {
   // Copy badge embed link to clipboard
@@ -182,19 +230,19 @@ function initContactPageClients() {
     "recommended_clients_list"
   );
   if (recommended_clients_list) {
-    const _filter_clients = function (os_name) {
+    const filterClients = function (os_name) {
       for (const client of recommended_clients_list.children) {
         client.hidden = os_name != client.getAttribute("data-os");
       }
     };
     if (navigator.userAgent.indexOf("Android") >= 0) {
-      _filter_clients("Android");
+      filterClients("Android");
     } else if (navigator.userAgent.indexOf("Linux") >= 0) {
-      _filter_clients("Linux");
+      filterClients("Linux");
     } else if (navigator.userAgent.indexOf("iPhone") >= 0) {
-      _filter_clients("iPhone");
+      filterClients("iPhone");
     } else {
-      _filter_clients("Windows");
+      filterClients("Windows");
     }
   }
 }
